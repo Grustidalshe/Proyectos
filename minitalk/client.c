21a6891146016c072f76d3e18a84c347794b1e58
Grustidalshe <111081934+Grustidalshe@users.noreply.github.com>
2024-05-07 03:52:30 +0300

    Update client.c

--- minitalk/client.c
+++ minitalk/client.c
@@ -12,29 +12,31 @@
 
 #include ""
 
-static void	manda(int pid, unsigned char *sq)
+static void	ft_send_singal(int pid, char *lin)
 {
+	int		bit;
 	unsigned char	u;
-
-	u = 128;
-	while (u > 0)
+	
+	while (*lin)
 	{
-		if (octet & u)
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
-		u >>= 1;
-		usleep(500);
+		bit = 8;
+		u = *lin;
+		while(bit--)
+		{
+			if ((u >> bit) & 1)
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			usleep(200);
+		}
+		lin++;
 	}
 }
 
 int	main(int argc, char **argv)
 {
-	int	pid;
-
 	if (argc != 3)
 		return (-1);
-	pid = ft_atoi(argv[1]);
-	manda(argv[2], pid);
+	ft_send_singal(ft_atoi(argv[1]), argv[2]);
 	return (0);
 }
